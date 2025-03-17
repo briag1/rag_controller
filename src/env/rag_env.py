12b93ec9588670af20359cda11e14aa0c6db92a2
briag1 <briag.rehel@hotmail.fr>
2025-03-17 23:36:51 +0100

    fix: Add a num actions param to env.

--- src/env/rag_env.py
+++ src/env/rag_env.py
@@ -7,14 +7,14 @@ from src.dataset.base import RAGDataset
 from src.model.rag.adaptative_top_k import AdaptiveTopKQueryEngine
 from src.env.rewarder.base import Rewarder
 class RAGEnv(gym.Env):
-    def __init__(self, dataset: RAGDataset, rewarder: Rewarder, embedding_model: str, llm: str,):
+    def __init__(self, dataset: RAGDataset, rewarder: Rewarder, embedding_model: str, llm: str, num_top_k_values:int = 5):
         super().__init__()
         self.dataset = dataset.qa_dataset
         self.num_samples = len(self.dataset)
         # Define observation and action spaces
         max_question_length = max(len(sample["question"]) for sample in self.dataset)
         self.observation_space = gym.spaces.Text(max_length=max_question_length)
-        self.action_space = gym.spaces.Discrete(4)  # top_k values: {1, 3, 5, 10, 20}
+        self.action_space = gym.spaces.Discrete(num_top_k_values)  # top_k values: {1, 3, 5, 10, 20}
         self.rag = AdaptiveTopKQueryEngine.build(dataset.documents, embedding_model, llm)
         self.current_index = 0  # Random question index
         self.rewarder = rewarder
@@ -32,8 +32,8 @@ class RAGEnv(gym.Env):
         reference_answer = self.dataset[self.current_index]["answer"]
 
         # Convert action index to top_k value: {0 → 1, 1 → 3, 2 → 5, 3 → 10, 4 → 20}
-        top_k_values = [0, 1, 2, 3]
-        top_k = top_k_values[action]
+        #top_k_values = 
+        top_k = action
 
         # Get retrieved documents and compute relevance
         answer = self.rag.query_with_config(question, top_k)
