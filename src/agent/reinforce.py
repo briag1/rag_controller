fix: Mean computation.

src/agent/reinforce.py
@@ -52,8 +52,8 @@ class Reinforce(Agent):
                 continue
             current_return = self.gamma * current_return + reward
             prob = self.encoder(input_ids = obs["input_ids"].unsqueeze(0), attention_mask = obs["attention_mask"].unsqueeze(0)).logits.softmax(1)[action]
-            p_loss += torch.log(prob)*current_return
-        p_loss = -1/(len(rewards)-1)*p_loss
+            p_loss += torch.log(prob)* current_return
+        p_loss = -p_loss/len(rewards)
         p_loss.backward()
         self.encoder_optim.step()
         self.encoder_optim.zero_grad()